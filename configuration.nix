a8b0a6aef6ad6461e1f7ec04421ad7d9105d0c3f
Young <young20050727@gmail.com>
2025-08-02 14:09:25 +0800

    fix: fill actual sha256 value

--- configuration.nix
+++ configuration.nix
@@ -12,7 +12,7 @@ let
   hmTarball = builtins.fetchTarball {
     url = "https://github.com/nix-community/home-manager/archive/refs/heads/release-25.05.tar.gz";
     # 請改成正確的 SRI 值（例如 "sha256-xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx="）
-    sha256 = lib.fakeSha256;
+    sha256 = "sha256-026rvynmzmpigax9f8gy9z67lsl6dhzv2p6s8wz4w06v3gjvspm1";
   };
 in
 {
