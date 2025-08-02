233da26307f86661fb0495fc2a0b8e70f71fd7bf
Young <young20050727@gmail.com>
2025-08-02 13:59:54 +0800

    feat: add sha256

--- configuration.nix
+++ configuration.nix
@@ -10,7 +10,7 @@
 
     "${builtins.fetchTarball {
       url = "https://github.com/NixOS/nixpkgs/archive/refs/tags/25.05.tar.gz";
-      sha256 = "0000000000000000000000000000000000000000000000000000";
+      sha256 = "1915r28xc4znrh2vf4rrjnxldw2imysz819gzhk9qlrkqanmfsxd";
     }}/nixos"
   ];
 
