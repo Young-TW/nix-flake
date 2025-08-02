7a7b7fb3155c1f19f563953ac5d62fe864ae698d
Young <young20050727@gmail.com>
2025-08-02 14:06:03 +0800

    fix(nixos): stop importing nixpkgs as a module; pin nixpkgs 25.05 and add Home Manager via tarball

--- configuration.nix
+++ configuration.nix
@@ -1,17 +1,27 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
+# /etc/nixos/configuration.nix
+{ config, lib, pkgs, ... }:
 
-{ config, pkgs, ... }:
+let
+  # 釘住 nixpkgs 至 25.05（使用你先前提供的 base32 雜湊，OK）
+  pinnedPkgs = import (builtins.fetchTarball {
+    url = "https://github.com/NixOS/nixpkgs/archive/refs/tags/25.05.tar.gz";
+    sha256 = "1915r28xc4znrh2vf4rrjnxldw2imysz819gzhk9qlrkqanmfsxd";
+  }) { };
 
+  # 釘住 Home Manager（release-25.05）
+  hmTarball = builtins.fetchTarball {
+    url = "https://github.com/nix-community/home-manager/archive/refs/heads/release-25.05.tar.gz";
+    # 請改成正確的 SRI 值（例如 "sha256-xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx="）
+    sha256 = "sha256-請換成正確的SRI雜湊值";
+  };
+in
 {
+  # 讓整個系統使用 pinnedPkgs（之後 config 內的 pkgs 都來自這個釘版）
+  nixpkgs.pkgs = pinnedPkgs;
+
   imports = [
     ./hardware-configuration.nix
-
-    "${builtins.fetchTarball {
-      url = "https://github.com/NixOS/nixpkgs/archive/refs/tags/25.05.tar.gz";
-      sha256 = "1915r28xc4znrh2vf4rrjnxldw2imysz819gzhk9qlrkqanmfsxd";
-    }}/nixos"
+    (import "${hmTarball}/nixos")   # 匯入 Home Manager NixOS 模組
   ];
 
   home-manager = {
@@ -31,7 +41,6 @@
     };
   };
 
-
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
@@ -39,22 +48,12 @@
   # Use latest kernel.
   boot.kernelPackages = pkgs.linuxPackages_latest;
 
-  networking.hostName = "nixos"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Enable networking
+  networking.hostName = "nixos";
   networking.networkmanager.enable = true;
 
-  # Set your time zone.
   time.timeZone = "Asia/Taipei";
 
-  # Select internationalisation properties.
   i18n.defaultLocale = "en_US.UTF-8";
-
   i18n.extraLocaleSettings = {
     LC_ADDRESS = "zh_TW.UTF-8";
     LC_IDENTIFICATION = "zh_TW.UTF-8";
@@ -69,23 +68,18 @@
 
   services.openssh.enable = true;
 
-  # Enable the X11 windowing system.
+  # X11 與 GNOME
   services.xserver.enable = true;
-
-  # Enable the GNOME Desktop Environment.
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
-
-  # Configure keymap in X11
   services.xserver.xkb = {
     layout = "us";
     variant = "";
   };
 
-  # Enable CUPS to print documents.
   services.printing.enable = true;
 
-  # Enable sound with pipewire.
+  # 音訊（PipeWire）
   services.pulseaudio.enable = false;
   security.rtkit.enable = true;
   services.pipewire = {
@@ -93,18 +87,9 @@
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
+    # jack.enable = true;
   };
 
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
-
-  # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.young = {
     isNormalUser = true;
     description = "young";
@@ -124,14 +109,10 @@
     ];
   };
 
-  # Install firefox.
   programs.firefox.enable = true;
 
-  # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
 
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
   environment.systemPackages = with pkgs; [
     vim
     wget
@@ -160,31 +141,5 @@
     eza
   ];
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "25.05"; # Did you read the comment?
-
+  system.stateVersion = "25.05";
 }
