48a0b6b0b42a3ef3c2175ce15ca52bd4ee9095b6
young <me@youn.gg>
2025-08-04 17:14:52 +0800

    add podman

--- modules/apps.nix
+++ modules/apps.nix
@@ -4,5 +4,6 @@
     cava docker vim wget fish fastfetch btop tmux curl htop bat
     rustc cargo git github-cli cmake clang yt-dlp
     neovim ranger onefetch cxxopts fmt ibus jetbrains-mono eza
+    podman podman-compose
   ];
 }
