d05eceff0428e9f35c52693387ddce15dd6b497d
Young <young20050727@gmail.com>
2025-08-22 05:09:44 +0800

    remove RDP & VNC

--- home/young/home.nix
+++ home/young/home.nix
@@ -1,19 +1,3 @@
 { config, pkgs, ... }:
 {
-  dconf.settings = {
-    "org/gnome/desktop/remote-desktop" = {
-      # 二擇一或都開
-      rdp-enable = true;   # RDP 建議
-      vnc-enable = true;  # 需要 VNC 時改成 true
-    };
-    # RDP 細節（可選）
-    "org/gnome/desktop/remote-desktop/rdp" = {
-      screen-share-mode = "extend";   # or "mirror"
-      view-only = false;
-    };
-    # VNC 細節（若啟用 VNC）
-    "org/gnome/desktop/remote-desktop/vnc" = {
-      view-only = false;
-    };
-  };
 }
